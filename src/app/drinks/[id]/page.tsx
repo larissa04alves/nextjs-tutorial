6471ecfd64d20c8b6ab78f9811793609f475cc07
Larissa <alves04.larissa@gmail.com>
2025-02-28 17:36:09 -0300

    feat: listagem drinks API externa

--- src/app/drinks/[id]/page.tsx
+++ src/app/drinks/[id]/page.tsx
@@ -1,4 +1,17 @@
-import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import imgDrink from './imgDrink.jpg';
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getSingleDrink = async (id: string): Promise<any> => {
+    const res = await fetch(`${url}${id}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch a drink...');
+    }
+    return res.json();
+};
 
 interface SingleDrinkPageProps {
     params: {
@@ -6,11 +19,18 @@ interface SingleDrinkPageProps {
     };
 }
 
-const SingleDrinkPage: FC<SingleDrinkPageProps> = ({ params }) => {
-    console.log(params.id);
-
+const SingleDrinkPage = async ({ params }: SingleDrinkPageProps) => {
+    const data = await getSingleDrink(params.id);
+    const title = data?.drinks?.[0]?.strDrink;
+    const imgSrc = data?.drinks?.[0]?.strDrinkThumb;
     return (
-        <div>SingleDrinkPage</div>
+        <div>
+            <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
+                Back to drinks
+            </Link>
+            <h1 className='text-4xl mb-8'>{title}</h1>
+            <Image src={imgDrink} className='w-56 h-5w-56 rounded' alt='drink' />
+        </div>
     );
 };
 
